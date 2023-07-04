ec4458cf405a9bcb436a08c0a06d7ddbcd66e74b
LEE EUNJI <eunjilee3008@gmail.com>
2023-07-04 11:22:57 +0900

    API 테스트로 전환하기

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,21 +1,39 @@
 package com.example.productorderservice.product;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootTest
-class ProductServiceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductApiTest extends ApiTest {
 
     @Autowired
     private ProductService productService;
 
     @Test
     void 상품등록() {
-        final AddProductRequest request = 상품등록요청_생성();
+        final var request = 상품등록요청_생성();
 
-        productService.addProduct(request);
+        //API 요청
+        final var response = 상품등록요청(request);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private static ExtractableResponse<Response> 상품등록요청(final AddProductRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
     }
 
     private static AddProductRequest 상품등록요청_생성() {
