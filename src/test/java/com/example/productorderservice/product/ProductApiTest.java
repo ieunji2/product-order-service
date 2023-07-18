959ad2d75ad1f9bde3b347805e467c83f950c492
LEE EUNJI <eunjilee3008@gmail.com>
2023-07-18 10:46:23 +0900

    API 테스트로 전환하기

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -1,12 +1,18 @@
 package com.example.productorderservice.product;
 
 import com.example.productorderservice.ApiTest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductApiTest extends ApiTest {
+
+    @Autowired
+    ProductRepository productRepository;
 
     @Test
     void 상품등록() {
@@ -21,12 +27,24 @@ class ProductApiTest extends ApiTest {
     @Test
     void 상품조회() {
         ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
-        Long productId = 1L;
+        final Long productId = 1L;
 
         final var response = ProductSteps.상품조회요청(productId);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+    }
+
+    @Test
+    void 상품수정() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+
+        final Long productId = 1L;
+        final UpdateProductRequest request = ProductSteps.상품수정요청_생성();
+        final ExtractableResponse<Response> response = ProductSteps.상품수정요청(productId, request);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(productRepository.findById(productId).get().getName()).isEqualTo("상품 수정");
     }
 
 }
