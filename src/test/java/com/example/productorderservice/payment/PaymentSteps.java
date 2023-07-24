e2c0df7065889c0f5386c11cb1eb55f2f23a135a
LEE EUNJI <eunjilee3008@gmail.com>
2023-07-24 13:46:05 +0900

    스프링부트 테스트로 전환하기

--- src/test/java/com/example/productorderservice/payment/PaymentSteps.java
+++ src/test/java/com/example/productorderservice/payment/PaymentSteps.java
@@ -1,7 +1,7 @@
 package com.example.productorderservice.payment;
 
 public class PaymentSteps {
-    public static PaymentRequest 상품주문결제_요청() {
+    public static PaymentRequest 주문결제_요청() {
         final Long orderId = 1L;
         final String cardNumber = "1234-1234-1234-1234";
         return new PaymentRequest(orderId, cardNumber);
