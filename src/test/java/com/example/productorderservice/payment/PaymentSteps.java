6b06d331f69b8021102da5c6b1946664dab00217
LEE EUNJI <eunjilee3008@gmail.com>
2023-07-24 11:18:12 +0900

    POJO 주문 결제 기능 구현하기

---
+++ src/test/java/com/example/productorderservice/payment/PaymentSteps.java
@@ -0,0 +1,9 @@
+package com.example.productorderservice.payment;
+
+public class PaymentSteps {
+    public static PaymentRequest 상품주문결제_요청() {
+        final Long orderId = 1L;
+        final String cardNumber = "1234-1234-1234-1234";
+        return new PaymentRequest(orderId, cardNumber);
+    }
+}
